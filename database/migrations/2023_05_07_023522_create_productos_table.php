d55ccab4bdb8167d4736ae73e507c8d42d5bb92e
rido60 <jrmjrido@gmail.com>
2023-05-07 00:02:43 -0400

    migraciones y modelos

---
+++ database/migrations/2023_05_07_023522_create_productos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            $table->string("nombre", 30);
+            $table->decimal("precio", 10,2)->default(0);
+            $table->integer("cantidad");
+            $table->integer("stock")->default(0);
+            $table->boolean("estado")->default(true);
+            $table->string("imagen")->nullable();
+            $table->text("descripcion")->nullable();
+            //N:1
+            $table->bigInteger("categoria_id")->unsigned();
+            $table->foreign("categoria_id")->references("id")->on("categorias");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
---
+++ docs/comandos de GIT.md
@@ -0,0 +1,4 @@
+# 1. descargar e instalat GIT.md
+GIT sistema de versiones 
+
+# 2. configuarar GIT
