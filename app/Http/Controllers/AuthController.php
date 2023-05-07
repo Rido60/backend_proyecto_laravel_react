538ca0b24dcbceb907cac3eec4c233ffa858995d
rido60 <jrmjrido@gmail.com>
2023-05-06 22:04:36 -0400

    Login api Rest Laravel

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        # code...
+        $credenciales = $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+
+        if (!Auth::attempt($credenciales)){
+            return response()->json(["messaje"=> "Usuario no Correcto"],401);
+        }
+
+        // generar e token con sactum
+        $user = Auth::user();
+        $token = $user->createtoken("token personal")->plainTextToken;
+
+        // responder
+        return response()->json([
+            "access_token"=> $token,
+            "token_type"=> "Bearer",
+            "usuario"=>$user
+        ]);
+    }
+
+    public function register(Request $request)
+    {
+        # validar
+        $request->validate([
+            "name" => "required",
+            "email" => "required|email|unique:users,email",
+            "password" => "required"
+        ]);
+        // si falla no pasa al siguiente(guardar) y da error de 422
+        //guardar
+        $usuario = new User;  //instancia
+        $usuario->name = $request->name ;
+        $usuario->email = $request->email;
+        $usuario->password = bcrypt($request->password);
+
+        $usuario->save();
+
+        //return
+        return response()->json(["message"=> "Usuario Registrado"],201);
+    }
+
+    public function miPerfil()
+    {
+        # perfil
+        $user = Auth::user();
+        return response()->json($user);
+    }
+
+    public function salir()
+    {
+        # salir
+        Auth::user()->tokens()->delete();
+        return response()->json(["message"=> "Salir"]);
+    }
+}
+
+
